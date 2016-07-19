d07abbac7c1a19a0b7baa69474d11c77f7f78883
Tuyet Le Thi Anh <tuyet.le@pyramid-consulting.com>
2016-07-19 15:18:08 +0700

    Add step-2-1

--- src/app/store/todoStore.js
+++ src/app/store/todoStore.js
@@ -1,8 +1,17 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import todoApp from '../reducers/todoApp';
 
+const logger = (store) => (next) => (action) => {
+	console.log('prev state:', store.getState());
+	console.log('dispatching:', action);
+	let result = next(action);
+	console.log('next state', store.getState());
+  return result
+};
+
 const todoStore  = createStore(
-	todoApp
+	todoApp,
+	applyMiddleware(logger)
 );
 
 export default todoStore;
