bd3d9714488d1b4091448a9a503dd3f7b58997ec
Tuyet Le Thi Anh <tuyet.le@pyramid-consulting.com>
2016-07-19 11:01:06 +0700

    Update step1

--- src/app/components/AddTodo.js
+++ src/app/components/AddTodo.js
@@ -1,31 +1,31 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
 import { connect } from 'react-redux';
 import todoActionCreators from '../actions/todoActionCreators';
 
-let AddTodo = ({ dispatch }) => {
-  let input
+class AddTodo extends Component { 
+  render() {
+    return (
+      <div className="input-form">
+        <form onSubmit={e => {
+          e.preventDefault()
+          if (!this.refs.text.value.trim()) {
+            return
+          }
+          this.props.dispatch(todoActionCreators.addTodo(this.refs.text.value))
+          this.refs.text.value = ''
+        }}>
+          <input type="text" ref="text" placeholder="Enter Text" />
+          <button type="submit" className="btn-add">
+            Add Todo
+          </button>
+        </form>
+      </div>
+    );
+  }
+} 
 
-  return (
-    <div className="input-form">
-      <form onSubmit={e => {
-        e.preventDefault()
-        if (!input.value.trim()) {
-          return
-        }
-        dispatch(todoActionCreators.addTodo(input.value))
-        input.value = ''
-      }}>
-        <input ref={node => {
-          input = node
-        }} />
-        <button type="submit" className="btn-add">
-          Add Todo
-        </button>
-      </form>
-    </div>
-  )
+AddTodo.PropTypes = {
+  text: PropTypes.string
 }
 
-AddTodo = connect()(AddTodo)
-
-export default AddTodo;
+export default connect()(AddTodo);
